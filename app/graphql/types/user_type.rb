a22a622154c2071bbd0ea30966cb5d030de0f3b5
Anuj Biyani <305254+anujbiyani@users.noreply.github.com>
2020-12-02 16:43:29 -0700

    add another scenario to show how the double query problem...isn't a problem

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,10 +4,14 @@ module Types
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
-    field :posts, Types::PostType.connection_type, null: true
-    def posts
-      # object.posts
+    field :posts_with_batch_loading, Types::PostType.connection_type, null: true
+    def posts_with_batch_loading
       AssociationLoader.for(User, :posts).load(object)
+    end
+
+    field :posts_without_batch_loading, Types::PostType.connection_type, null: true
+    def posts_without_batch_loading
+      object.posts
     end
   end
 end
--- spec/graphql/query_spec.rb
+++ spec/graphql/query_spec.rb
@@ -29,26 +29,52 @@ RSpec.describe 'Query tests' do
       }
     GRAPHQL
 
-    execute_query(query)
+    p execute_query(query)
   end
 
-  # This scenario should be able to paginate the posts for one user, but it results in two queries:
+  # This scenario paginates the posts for one user, but it does result in an extra query:
   # one that is unpaginated and fetches all posts for a user,
   # and another that is paginated and fetches all posts for a user
-  it 'user and posts' do
+  it 'user and posts, with batch loading' do
     user_one = FactoryBot.create(:user)
     FactoryBot.create_list(:post, 3, user: user_one)
 
     query = <<~GRAPHQL
       query {
         user {
-          posts(first: 2) {
+          postsWithBatchLoading(first: 2) {
             nodes { id }
           }
         }
       }
     GRAPHQL
 
-    execute_query(query)
+    p execute_query(query)
+
+    # generated SQL:
+    #   User Load (0.1ms)  SELECT "users".* FROM "users" ORDER BY "users"."id" ASC LIMIT ?  [["LIMIT", 1]]
+    #   Post Load (0.1ms)  SELECT "posts".* FROM "posts" WHERE "posts"."user_id" = ?  [["user_id", 1]]
+    #   Post Load (0.1ms)  SELECT "posts".* FROM "posts" WHERE "posts"."user_id" = ? LIMIT ?  [["user_id", 1], ["LIMIT", 2]]
+  end
+
+  it 'user and posts, without batch loading' do
+    user_one = FactoryBot.create(:user)
+    FactoryBot.create_list(:post, 3, user: user_one)
+
+    query = <<~GRAPHQL
+      query {
+        user {
+          postsWithoutBatchLoading(first: 2) {
+            nodes { id }
+          }
+        }
+      }
+    GRAPHQL
+
+    p execute_query(query)
+
+    # generated SQL:
+    #   User Load (0.1ms)  SELECT "users".* FROM "users" ORDER BY "users"."id" ASC LIMIT ?  [["LIMIT", 1]]
+    #   Post Load (0.1ms)  SELECT "posts".* FROM "posts" WHERE "posts"."user_id" = ? LIMIT ?  [["user_id", 1], ["LIMIT", 2]]
   end
 end
